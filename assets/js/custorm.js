11fc11e716fa0fe275ec239507fb8074d03fe245
Azmir Hossain <45963947+azmir-hasan@users.noreply.github.com>
2020-06-10 23:38:15 +0600

    Add files via upload

---
+++ assets/js/custorm.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------*
+         * Owl Carousel
+         ---------------------------------------------*/
+
+        $('.product-feature-carousel').owlCarousel({
+            loop: true,
+            autoplay: true,
+            nav: false,
+            items: 1,
+            dots: true,
+            smartSpeed: 750,
+        });
